f2e74f4e858b64b101f8d1eb98eed0af18b007f2
justin cumbus <justin.cumbus@gmail.com>
2017-04-29 19:37:44 +0000

    added hard and easy mode buttons

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
-var colors = createColors();
+var colors = createColors(6);
 var pickedColor = pickRandomColor(colors);
 
 
 var message = document.getElementById("message");
 var squares = document.querySelectorAll(".square");
 var colorDisplay = document.querySelector("#colorDisplay");
+
+var easyButton = document.querySelector("#easyButton");
+var hardButton = document.querySelector("#hardButton");
+
+easyButton.addEventListener("click", function () {
+	if (!easyButton.classList.contains("selected")){
+	easyButton.classList.toggle("selected");
+	}
+});
+
+hardButton.addEventListener("click", function () {
+	alert("hard button clicked");	
+	
+});
+
+
 var resetButton = document.querySelector("#resetButton");
 resetButton.addEventListener("click", function() {
-	colors = createColors();
+	colors = createColors(6);
 	pickedColor = pickRandomColor(colors);
 	colorDisplay.textContent = pickedColor;
+	message.textContent = "";
 for (var i = 0; i < squares.length; i++) {
 	squares[i].style.background = colors[i].rgb;
 }
 
+
+document.querySelector("H1").style.background =  "#232323"; ;
+
 });
 colorDisplay.textContent = pickedColor;
 
-function createColors() {
+function createColors(num) {
 	var colorArray = [];
 	var count = 0;
 	var r = 0;
 	var g = 0;
 	var b = 0;
 
-	while (count < 6) {
+		while (count < num) {
 
 		r = ("(".concat(Math.floor(Math.random() * (256 - 0)) + 0)).concat(", ");
 		g = (Math.floor(Math.random() * (256 - 0)) + 0) + ", ";
